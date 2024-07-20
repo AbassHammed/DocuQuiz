cd37442d66382daf040be08b6a873d32445ca021
Abass Hammed <abasshammedola@gmail.com>
2024-07-20 23:53:57 +0200

    Setting up the header component

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import { Navbar } from '@components';
+
+export default function Home() {
+  return (
+    <main className="relative">
+      <Navbar />
+    </main>
+  );
+}
---
+++ src/components/index.tsx
@@ -0,0 +1,2 @@
+export { default as ProgressBar } from './generating';
+export { default as Navbar } from './sections/header';
