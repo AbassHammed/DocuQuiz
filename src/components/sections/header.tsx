d2b159c15132816f1aac92e546376d10a13bb832
Abass Hammed <abasshammedola@gmail.com>
2024-07-21 00:49:54 +0200

    Added some icons

--- src/components/sections/header.tsx
+++ src/components/sections/header.tsx
@@ -17,27 +17,11 @@ export default function Navbar() {
       className={`bg-white fixed top-0 left-0 w-full h-20 duration-200 ease-[cubic-bezier(0,0,0,1)] lg:border-b border-zinc-100 text-zinc-600 z-20 ${
         !toggleMenu ? 'overflow-hidden' : 'bg-white md:h-20 h-full overflow-visible'
       }`}>
-      <div className="max-w-7xl mx-auto flex items-center justify-between lg:px-8 px-6 py-6">
-        <Link
-          href="/"
-          className="font-geistmono font-bold text-lg flex items-center gap-x-2 order-2">
-          <Image src={Logo} alt="Reda logo" width={20} />
-          Quiznote
+      <div className="max-w-7xl flex items-center justify-between p-4">
+        <Link href="/" className="font-bold text-lg flex items-center gap-x-2 order-2">
+          <Image src={Logo} alt="Reda logo" width={40} height={40} quality={95} priority />
+          DocuQuiz
         </Link>
-
-        {/* <nav className="md:block hidden">
-          <ul className="flex items-center gap-3 pl-4 order-1">
-            {navlinks.map(link => (
-              <li key={link.name}>
-                <Link
-                  href={link.url}
-                  className="font-medium text-sm tracking-tight text-zinc-500 hover:text-zinc-700 duration-300">
-                  {link.name}
-                </Link>
-              </li>
-            ))}
-          </ul>
-        </nav> */}
 
         {/* <div className="order-3">
           <div className="md:flex hidden items-center gap-x-6">
@@ -49,10 +33,8 @@ export default function Navbar() {
               theme="primary"
             />
           </div>
-          <HamburgerButton toggleMenu={toggleMenu} onToggle={setToggleMenu} />
         </div> */}
       </div>
-      {/* <MobileMenu toggleMenu={toggleMenu} onToggle={setToggleMenu} /> */}
     </header>
   );
 }
