f92180558671688746f247aa4af9fc7c50f57bb8
Abass Hammed <abasshammedola@gmail.com>
2024-07-22 06:52:29 +0200

    Some test again

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable quotes */
 'use client';
 
 import { useEffect, useState } from 'react';
@@ -6,18 +7,37 @@ import { ProgressBar } from '@components';
 
 const Loading = () => {
   const [value, setValue] = useState(0);
+  const [messageIndex, setMessageIndex] = useState(0);
+
+  const messages = [
+    'Envoi de vos données pour analyse...',
+    'Génération du quiz en cours...',
+    'Presque terminé...',
+    "Nous sommes désolés pour l'attente...",
+  ];
 
   useEffect(() => {
+    const handleMessageIndex = () => {
+      setMessageIndex(prevIndex => (prevIndex + 1) % messages.length);
+    };
+
     const handleIncrement = (prev: number) => {
       if (prev === 100) {
         return 0;
       }
+
+      if (prev % 20 === 0) {
+        handleMessageIndex();
+      }
       return prev + 10;
     };
+
     setValue(handleIncrement);
-    const interval = setInterval(() => setValue(handleIncrement), 2000);
+    const interval = setInterval(() => {
+      setValue(handleIncrement);
+    }, 2000);
     return () => clearInterval(interval);
-  }, []);
+  }, [messages.length]);
 
   return (
     <div className="flex flex-col items-center space-y-8">
@@ -30,7 +50,7 @@ const Loading = () => {
       />
 
       <h2 className="font-semibold tracking-tight text-center text-zinc-600">
-        Generating quiz ...
+        {messages[messageIndex]}
       </h2>
     </div>
   );
--- src/components/sections/formContainer.tsx
+++ src/components/sections/formContainer.tsx
@@ -2,7 +2,7 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 'use client';
 
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 import { HomeForm, Loading } from '@components';
 import { useFormStore, useQuizStore, useTimerStore } from '@store';
@@ -16,37 +16,47 @@ const FormContainer = () => {
   const setTimer = useTimerStore(state => state.setTimer);
   const [resContent, setResContent] = useState('');
 
+  useEffect(() => {
+    console.log(resContent);
+  }, [resContent]);
+
   const onSubmit = async (data: FormSubmitType) => {
-    setStatus('streaming');
-    setResContent('');
+    try {
+      setStatus('streaming');
+      setResContent('');
 
-    const formData = new FormData();
+      const formData = new FormData();
+      formData.append('topic', data.topic);
+      formData.append('text', data.text);
+      formData.append('difficulty', data.difficulty);
+      formData.append('quizCount', data.quizCount);
+      formData.append('timer', data.timer);
 
-    formData.append('topic', data.topic);
-    formData.append('text', data.text);
-    formData.append('difficulty', data.difficulty);
-    formData.append('quizCount', data.quizCount);
-    formData.append('timer', data.timer);
+      const response = await fetch('/api/server', {
+        method: 'POST',
+        body: formData,
+      });
 
-    const res = await fetch('/api/server', {
-      method: 'POST',
-      body: formData,
-    });
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
 
-    if (res.ok) {
-      const result = await res.json();
-      console.log(result);
-    } else {
-      console.error('Error submitting form');
+      const responseData = await response.text();
+      const formattedContent = responseData.replace('```json', '').replace('```', '');
+      setResContent(formattedContent);
+    } catch (error) {
+      console.error('Error:', error);
+      setResContent('An error occurred while processing your request.');
+    } finally {
+      console.log('Error hello :', resContent);
+      setStatus('done');
     }
-
-    setStatus('done');
   };
 
   return (
     <section className="relative bg-gray-100 border border-zinc-100 rounded-2xl max-w-4xl lg:mx-auto mx-4 lg:p-12 p-6 mb-10 min-h-72 ring-1 ring-gray-300">
-      {/* {status === 'idle' && <HomeForm onSubmit={onSubmit} />} */}
-      {status === 'idle' && <Loading />}
+      {status === 'idle' && <HomeForm onSubmit={onSubmit} />}
+      {status === 'streaming' && <Loading />}
     </section>
   );
 };
