e5a9875a168a3bb94a0a4baf88aae5f584673afb
Abass Hammed <abasshammedola@gmail.com>
2024-07-23 02:36:17 +0200

    Updated some data

--- src/config/site.ts
+++ src/config/site.ts
@@ -1,7 +1,7 @@
 export const siteConfig = {
   name: 'DocuQuiz',
-  url: 'https://docuquiz.hammedabass.com',
-  ogImage: 'https://docuquiz.hammedabass.com/og.jpg',
+  url: 'https://docuquiz.netlify.app/',
+  ogImage: 'https://docuquiz.netlify.app/og.png',
   description: 'Transform Your Documents into Engaging Quizzes',
   links: {
     twitter: 'https://twitter.com/abasshammedola',
